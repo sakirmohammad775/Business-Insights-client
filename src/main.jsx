9f4be72adf40e967167a09d020925b9a413879a0
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-04-18 15:03:31 +0600

    started with using tailwind and daisyUi and installation necessary file

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Navbar from './Shared/Navbar/Navbar.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar></Navbar>,
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>,
+)
